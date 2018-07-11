cd76eac6b4db868bec00269537481b207f756d66
hikaricore <hikaricore@gmail.com>
2018-07-11 00:13:15 -0400

    Update trailer.sh

--- trailer.sh
+++ trailer.sh
@@ -25,11 +25,11 @@ fi
 TT=$radarr_movie_imdbid
 
 # pull tmdb id for film based on imdbid (yes this does indeed require two api calls, and on rare occasions pulls the tmdb id of 6, probably due to the way I'm parsing the data in combination with old json formatting. happens with older films but seemingly not newer ones, or maybe it's random? idk. fixing this soon with jq instead of cut because sanity and stuff.)
-TMDB=$(curl -s "http://api.themoviedb.org/3/find/$TT?api_key=1a7373301961d03f97f853a876dd1212&language=en-US&external_source=imdb_id" | tac | tac | jq -r '.' | grep '"id:"' | sed 's/[^0-9]*//g')
+TMDB=$(curl -s "http://api.themoviedb.org/3/find/$TT?api_key=1a7373301961d03f97f853a876dd1212&language=en-US&external_source=imdb_id" | tac | tac | jq -r '.' | grep "id\"" | sed 's/[^0-9]*//g')
 #TMDB=$(curl -s "http://api.themoviedb.org/3/find/$TT?api_key=1a7373301961d03f97f853a876dd1212&language=en-US&external_source=imdb_id" | cut -d ':' -f6 | cut -d ',' -f1)
 
 # pull trailer video id from tmdb based on tmdb id (imperfect, may not grab anything or may grab an video that is not trailer)
-YOUTUBE=$(curl -s "http://api.themoviedb.org/3/movie/$TMDB/videos?api_key=1a7373301961d03f97f853a876dd1212" | tac | tac | jq -r '.' | grep key | cut -d '"' -f4)
+YOUTUBE=$(curl -s "http://api.themoviedb.org/3/movie/$TMDB/videos?api_key=1a7373301961d03f97f853a876dd1212" | tac | tac | jq -r '.' | grep key | cut -d \" -f4)
 #YOUTUBE=$(curl -s "http://api.themoviedb.org/3/movie/$TMDB/videos?api_key=1a7373301961d03f97f853a876dd1212" | cut -d ':' -f7 | cut -d '"' -f2)
 
 # download trailer from youtube based on video resolution (requires youtube-dl and permission to run it)
