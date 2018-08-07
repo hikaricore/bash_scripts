014725ce941b275e80d3caf352c68127826c73a9
hikaricore <hikaricore@gmail.com>
2018-08-07 02:06:55 +0000

    Update moviecrc.sh
    On second thought, let's not run into possible parsing errors by stripping the file extension.

--- moviecrc.sh
+++ moviecrc.sh
@@ -3,7 +3,8 @@ export IFS=$'\n' &&
 for i in $(ls */*.mkv */*.mp4 */*.avi */*.mpg */*.mpeg */*.webm */*.flv | grep -v movie-trailer); do
 
         crc=$(crc32 $i)
-        write=$(echo $i | sed s/\.[^.]*$//).crc
+        #write=$(echo $i | sed s/\.[^.]*$//).crc
+        write=$(echo $i).crc
         echo $crc >> $write
         # get the complete data sv from https://www.srrdb.com/open (requires login)
         #cat -v archivedfiles.csv | grep $(echo $crc | LC_ALL=sv_SE tr '[:lower:]' '[:upper:]') >> $write
