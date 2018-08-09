8b7632e67d37b245f709db7fd6a4636d39ebe839
hikaricore <hikaricore@gmail.com>
2018-08-09 02:19:25 +0000

    Update moviecrc.sh
    maybe added some checking for existing values, who knows
    
    incomplete mess of if then statements

--- moviecrc.sh
+++ moviecrc.sh
@@ -2,12 +2,64 @@
 export IFS=$'\n' &&
 for i in $(ls */*.mkv */*.mp4 */*.avi */*.mpg */*.mpeg */*.webm */*.flv | grep -v movie-trailer); do
 
-        crc=$(crc32 $i)
-        #write=$(echo $i | sed s/\.[^.]*$//).crc
-        write=$(echo $i).crc
-        echo $crc >> $write
-        # get the complete data sv from https://www.srrdb.com/open (requires login)
-        #cat -v archivedfiles.csv | grep $(echo $crc | LC_ALL=sv_SE tr '[:lower:]' '[:upper:]') >> $write
-        grep -f -i $crc archivedfiles.csv >> $write
+        # guess we should check for existing crc files so we don't waste time
+        # none of this has been tested, so have fun if something breaks horribly
+        # not even sure if these if tags are closed properly, seriously don't use this yet
+        
+        if [ ! -f $i.crc ]
+        
+                then
+                
+                        # if the crc file does not exist at all, we do these things
+                
+                        printf "CRC file does not exist for $i, creating one." >&2
+                        crc=$(crc32 $i)
+                        #write=$(echo $i | sed s/\.[^.]*$//).crc
+                        write="$i".crc
+                        echo $crc >> $write
+                        # get the complete data sv from https://www.srrdb.com/open (requires login)
+                        #cat -v archivedfiles.csv | grep $(echo $crc | LC_ALL=sv_SE tr '[:lower:]' '[:upper:]') >> $write
+                        grep -f -i $crc archivedfiles.csv >> $write
+                        
+                else
+                
+                        # see if the file has two lines
+                        if [[ $(wc -l "$i".crc) -eq 2 ]]
+                        #wc -l "$i".crc | cut -d\  -f1
+
+                                then
+                                
+                                        printf "CRC file exists for $i, attempting compare with release." >&2
+                                        crc=$(head -n 1 $i.crc)
+                                        #write=$(echo $i | sed s/\.[^.]*$//).crc
+                                        write="$i".crc
+                                        echo $crc > $write
+                                        # get the complete data sv from https://www.srrdb.com/open (requires login)
+                                        #cat -v archivedfiles.csv | grep $(echo $crc | LC_ALL=sv_SE tr '[:lower:]' '[:upper:]') >> $write
+                                        grep -f -i $crc archivedfiles.csv >> $write && echo "CRC Match!" >> $write
+                                # see if the file has one line
+                                else 
+                                if [[ $(wc -l "$i".crc) -eq 1 ]]
+                        
+                                        printf "CRC file exists for $i, attempting compare with release." >&2
+                                        crc=$(head -n 1 $i.crc)
+                                        #write=$(echo $i | sed s/\.[^.]*$//).crc
+                                        write="$i".crc
+                                        echo $crc > $write
+                                        # get the complete data sv from https://www.srrdb.com/open (requires login)
+                                        #cat -v archivedfiles.csv | grep $(echo $crc | LC_ALL=sv_SE tr '[:lower:]' '[:upper:]') >> $write
+                                        grep -f -i $crc archivedfiles.csv >> $write && echo "CRC Match!" >> $write
+                        
+                                # see if the file is already matched
+                                else
+                                if [[ $(wc -l "$i".crc) -eq 3 ]]
+                          
+                                        # we'll finish this mess later      
+                          
+                                fi
+                                
+                        fi
+                        
+        fi
         
 done
